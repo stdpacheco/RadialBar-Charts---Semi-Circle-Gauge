5685d118cd6dea93e80af15dccf348e3f2fedea8
stdpacheco <75811097+stdpacheco@users.noreply.github.com>
2023-04-06 10:17:48 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import {
   ApexPlotOptions,
   ApexChart,
   ApexFill,
+  ApexStroke,
+  ApexResponsive,
   ChartComponent
 } from "ng-apexcharts";
 
@@ -13,6 +15,9 @@ export type ChartOptions = {
   chart: ApexChart;
   labels: string[];
   plotOptions: ApexPlotOptions;
+  stroke: ApexStroke,
+  responsive: ApexResponsive[];
+  dataLabels: any;
   fill: ApexFill;
 };
 
@@ -24,53 +29,101 @@ export type ChartOptions = {
 export class AppComponent {
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
-
   constructor() {
     this.chartOptions = {
-      series: [60.99],
+      series: [60.05],
       chart: {
+        height: 350,
         type: "radialBar",
-        offsetY: -20
+        offsetY: -20,
+        toolbar: {
+          show: true
+        }
       },
       plotOptions: {
         radialBar: {
-          startAngle: -90,
-          endAngle: 90,
+          // startAngle: -90,
+          // endAngle: 90,     
+          startAngle: -115,
+          endAngle: 115,     
           track: {
-            background: "#e7e7e7",
+            background: "#EFEFEF",
             strokeWidth: "97%",
             margin: 5, // margin is in pixels
             dropShadow: {
+              // enabled: true,
+              // top: 2,
+              // left: 0,
+              // opacity: 0.31,
+              // blur: 2
               enabled: true,
-              top: 2,
+              top: -3,
               left: 0,
-              opacity: 0.31,
-              blur: 2
+              blur: 4,
+              opacity: 0.35
             }
           },
           dataLabels: {
+            show: true,
             name: {
-              show: true
+              offsetY: -33,
+              show: true,
+              color: '#888',
+              fontSize: '11px'
             },
             value: {
               offsetY: -2,
-              fontSize: "22px"
+              formatter(value:any):any {
+                return value+"%";
+              },
+              color: '#111',
+              fontSize: '30px',
+              fontFamily:"Nunito Sans",
+              fontWeight:"700",
+              show: true,
             }
-          }
+          }         
         }
       },
+      // fill: {
+      //   type: "gradient",
+      //   gradient: {
+      //     shade: "light",
+      //     shadeIntensity: 0.4,
+      //     inverseColors: false,
+      //     opacityFrom: 1,
+      //     opacityTo: 1,
+      //     stops: [0, 50, 53, 91]
+      //   }
+      // },
       fill: {
         type: "gradient",
         gradient: {
-          shade: "light",
-          shadeIntensity: 0.4,
-          inverseColors: false,
+          shade: "dark",
+          type: "horizontal",
+          shadeIntensity: 0.5,
+          gradientToColors: ["#ABE5A1"],
+          inverseColors: true,
           opacityFrom: 1,
           opacityTo: 1,
-          stops: [0, 50, 53, 91]
+          stops: [0, 100]
         }
       },
-      labels: ["Average Results 222"]
+      stroke: {
+        show: true,
+        lineCap: "round"
+      },
+      labels: ["PORCENTAJE PAGADO"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 100,
+            },
+          }
+        }
+      ]
     };
   }
 }
